0796f39760841e6cc7305e18d6c08abfa667f2c5
Murad Ziyabaev <ziyabaev_1996@mail.ru>
2025-02-24 20:52:08 +0500

    fix bug

--- main.py
+++ main.py
@@ -9,29 +9,20 @@ from main_router import router
 from openai_client import OpenAIService
 
 
-
-
-
-
-
-
-
 async def main():
     settings = Settings()
-    if not settings.ASSISTANT_ID:
-        await settings.init_assistant()
+    await settings.init_assistant()
 
-        if not settings.ASSISTANT_ID:
-            raise RuntimeError("Failed to create assistant")
+    if not settings.ASSISTANT_ID or settings.ASSISTANT_ID == "":
+        raise RuntimeError("Failed to create assistant")
     bot = Bot(settings.BOT_TOKEN)
     dp = Dispatcher()
-    client = OpenAIService(settings.ASSISTANT_ID,settings.OPENAI_API_KEY)
+    client = OpenAIService(settings.ASSISTANT_ID, settings.OPENAI_API_KEY)
     dp.update.middleware(ContextMiddleware(client, bot, settings))
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
     logging.info('Bot starting')
     await dp.start_polling(bot)
-
 
 
 if __name__ == "__main__":
